2513a116934490763e8769e8e028822858bea8ab
ketchupas2 <123992994+ketchupas2@users.noreply.github.com>
2024-10-30 15:55:03 +0200

    Update streak.lua

--- streak.lua
+++ streak.lua
@@ -1,9 +1,1 @@
--- test
-RegisterNetEvent('updateKillStreakUI')
-AddEventHandler('updateKillStreakUI', function(data)
-    SendNUIMessage({
-        type = 'updateKillStreakUI',
-        streak = data.streak
-    })
-end)
 
